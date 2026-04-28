9272a1c153c892348ffd87bcffa10b9c62a5096f
kyfasamarau-hash <kyfa.samar.au@phinmaed.com>
2026-04-28 18:20:27 +0800

    Delete Code directory

--- Code/script.js
+++
@@ -1,7 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-x')
-    navbar.classList.toggle('active')
-}
